67563c11e70c239219c67eb4f1cb7438e65d62de
Cintho <daconvertible0@gmail.com>
2026-03-25 01:37:25 +0100

    rendre le navbar responsive pour mobile

--- leagueofront/src/pages/index.jsx
+++ leagueofront/src/pages/index.jsx
@@ -1,26 +1,31 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import { useState } from 'react';
-import Navbar from '../components/Navbar';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Login from './login';
-
+import { useAuthStore } from '../store/authStore';
 
 export default function Home() {
-   const [activeTab, setActiveTab] = useState('ACCUEIL');
+  const router = useRouter();
+  const { token } = useAuthStore();
 
- 
+  useEffect(() => {
+    if (token) {
+      router.push('/Accueil');
+    }
+  }, [token, router]);
+
+  if (token) return null;
+
   return (
     <>
       <Head>
-        <title>League of Cards</title>
-        <meta name="description" content="League of Legends" />
+        <title>League of Stones</title>
+        <meta name="description" content="League of Stones - Jeu de cartes" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <Login />
-      
     </>
-    
   );
 }
